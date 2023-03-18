0d77142bf919870c9fe963fe404ceb89ef089ed2
SL6I <s443004498@st.uqu.edu.sa>
2023-03-18 23:31:26 +0300

    OOP in Java

---
+++ OOP/Car.java
@@ -0,0 +1,12 @@
+public class Car{
+    String make = "BMW";
+    String model = "IDK";
+    int year = 2020;
+     void drive(){
+        System.out.println("You drive the car");
+
+     }
+     void brake(){
+        System.out.println("You use the brake");
+     }
+}
---
+++ OOP/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Car car0 = new Car();
+        System.out.println(car.make);
+        System.out.println();
+        System.out.println(car0.make);
+        car0.drive();
+    }
+}
